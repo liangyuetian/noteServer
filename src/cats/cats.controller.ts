e95fbb8bab15278c1d0b92f862c3e7021026dda3
pt <pt_liangyue@outlook.com>
2022-04-04 22:27:20 +0800

    add cats demo

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
   @Get()
   hello() {
-    return 'hello cat';
+    return this.catsService.hello();
   }
 }
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,10 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 
 @Module({
   controllers: [CatsController],
+  providers: [CatsService],
 })
 export class CatsModule implements OnModuleInit {
   onModuleInit(): any {
