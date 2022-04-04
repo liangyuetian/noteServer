f2150e82df7cef8595ff55d79b44701a876fe2ed
pt <pt_liangyue@outlook.com>
2022-04-05 00:14:48 +0800

    add loggerServer

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, LoggerService } from '@nestjs/common';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
-  constructor(private catsService: CatsService) {}
+  constructor(
+    private catsService: CatsService,
+    private loggerService: LoggerService,
+  ) {}
   @Get()
   hello() {
+    this.loggerService.log('visit api /cats');
     return this.catsService.hello();
   }
 }
--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,17 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, LoggerService } from '@nestjs/common';
 import { NoteService } from './note.service';
 
 @Controller('note')
 export class NoteController {
-  constructor(private noteService: NoteService) {}
+  constructor(
+    private noteService: NoteService,
+    private loggerService: LoggerService,
+  ) {}
 
   @Get()
   hello() {
+    this.loggerService.log('visit api /note/hello');
     return this.noteService.hello();
   }
 
   @Get('list')
   getList() {
+    this.loggerService.log('visit api /note/list');
     return this.noteService.getList();
   }
 }
