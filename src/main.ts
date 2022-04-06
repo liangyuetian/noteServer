1a232b84df5311bf9c992806f35a37424b83f115
p t <pt_liangyue@outlook.com>
2022-04-06 14:57:43 +0000

    main.ts add hostname

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8200);
+  await app.listen(8200, '127.0.0.1');
   console.log('服务地址：', await app.getUrl());
 
   if (module.hot) {
